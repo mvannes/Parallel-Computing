126f28d7ed2179b80977d6c8612f08136ac830b7
Michael van Nes <michael.van.nes@hva.nl>
2017-04-20 20:56:27 +0200

    Steal a topological sort
    Stole a topological sort functions from the internet

---
+++ src/Graph.java
@@ -0,0 +1,67 @@
+// A Java program to print topological sorting of a DAG
+import java.util.*;
+
+// This class represents a directed graph using adjacency
+// list representation
+class Graph
+{
+    private int numberOfVertices;   // No. of vertices
+    private LinkedList<Integer> adjacencies[]; // Adjacency List
+
+    //Constructor
+    Graph(int numberOfVertices)
+    {
+        this.numberOfVertices = numberOfVertices;
+        adjacencies = new LinkedList[numberOfVertices];
+        for (int i=0; i < numberOfVertices; i++)
+            adjacencies[i] = new LinkedList();
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int vertice, int w) { adjacencies[vertice].add(w); }
+
+    // A recursive function used by topologicalSort
+    void topologicalSortUtil(int vertice, boolean visited[],
+                             Stack stack)
+    {
+        // Mark the current node as visited.
+        visited[vertice] = true;
+        Integer i;
+
+        // Recur for all the vertices adjacent to this
+        // vertex
+        Iterator<Integer> it = adjacencies[vertice].iterator();
+        while (it.hasNext())
+        {
+            i = it.next();
+            if (!visited[i])
+                topologicalSortUtil(i, visited, stack);
+        }
+
+        // Push current vertex to stack which stores result
+        stack.push(new Integer(vertice));
+    }
+
+    // The function to do Topological Sort. It uses
+    // recursive topologicalSortUtil()
+    public Stack topologicalSort()
+    {
+        Stack stack = new Stack();
+
+        // Mark all the vertices as not visited
+        boolean visited[] = new boolean[numberOfVertices];
+        for (int i = 0; i < numberOfVertices; i++) {
+            visited[i] = false;
+        }
+
+        // Call the recursive helper function to store
+        // Topological Sort starting from all vertices
+        // one by one
+        for (int i = 0; i < numberOfVertices; i++) {
+            if (visited[i] == false) {
+                topologicalSortUtil(i, visited, stack);
+            }
+        }
+       return stack;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Stack;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    // Driver method
+    public static void main(String args[])
+    {
+        // Create a graph with nodes.
+        Graph g = new Graph(6);
+        g.addEdge(5, 2);
+        g.addEdge(5, 0);
+        g.addEdge(4, 0);
+        g.addEdge(4, 1);
+        g.addEdge(2, 3);
+        g.addEdge(3, 1);
+
+        System.out.println("Following is a Topological " +
+                "sort of the given graph");
+        // Do a topological sort of the elements in the graph.
+        Stack sortedStack = g.topologicalSort();
+        while (!sortedStack.empty()) {
+            System.out.println(sortedStack.pop());
+        }
     }
 }
