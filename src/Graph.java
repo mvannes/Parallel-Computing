d54336054dc4b668423db876c99e1c1024fc7105
Michael van Nes <michael.van.nes@hva.nl>
2017-05-04 13:53:12 +0200

    Added Khan's algorithm, added timers
    Added a way to sort using Khan's algorithm, now also displays stats based
    on execution time of functions in nanotime

--- src/Graph.java
+++ src/Graph.java
@@ -1,25 +1,24 @@
 // A Java program to print topological sorting of a DAG
+
 import java.util.*;
 
 // This class represents a directed graph using adjacency
 // list representation
-class Graph
-{
+class Graph {
     private int numberOfVertices;   // No. of vertices
     private LinkedList<Integer> adjacencies[]; // Adjacency List
 
     //Constructor
-    public Graph(int numberOfVertices)
-    {
+    public Graph(int numberOfVertices) {
         this.numberOfVertices = numberOfVertices;
         adjacencies = new LinkedList[numberOfVertices];
-        for (int i=0; i < numberOfVertices; i++)
+        for (int i = 0; i < numberOfVertices; i++)
             adjacencies[i] = new LinkedList();
     }
 
     // Function to add an edge into the graph
     public void addEdge(int vertex, int target) {
-        if (adjacencies.length > vertex && adjacencies.length > target ) {
+        if (adjacencies.length > vertex && adjacencies.length > target) {
             adjacencies[vertex].add(target);
         } else {
             throw new RuntimeException("Either the vertex or the target for creation of a new edge was not found.");
@@ -28,8 +27,7 @@ class Graph
 
     // A recursive function used by topologicalSort
     private void topologicalSortUtil(int vertice, boolean visited[],
-                             Stack stack)
-    {
+                                     Stack stack) {
         // Mark the current node as visited.
         visited[vertice] = true;
         Integer i;
@@ -37,8 +35,7 @@ class Graph
         // Recur for all the vertices adjacent to this
         // vertex
         Iterator<Integer> it = adjacencies[vertice].iterator();
-        while (it.hasNext())
-        {
+        while (it.hasNext()) {
             i = it.next();
             if (!visited[i])
                 topologicalSortUtil(i, visited, stack);
@@ -48,15 +45,9 @@ class Graph
         stack.push(new Integer(vertice));
     }
 
-    public LinkedList<Integer>[] getAdjacencies()
-    {
-        return adjacencies;
-    }
-
     // The function to do Topological Sort. It uses
     // recursive topologicalSortUtil()
-    public Stack topologicalSort()
-    {
+    public Stack topologicalSort() {
         Stack stack = new Stack();
 
         // Mark all the vertices as not visited
@@ -73,6 +64,59 @@ class Graph
                 topologicalSortUtil(i, visited, stack);
             }
         }
-       return stack;
+        return stack;
+    }
+
+    // prints a Topological Sort of the complete graph using Khan's algorithm
+    public Stack topologicalSortKhan() {
+        // Create a array to store indegrees of all
+        // vertices. Initialize all indegrees as 0.
+        int inDegree[] = new int[numberOfVertices];
+
+        // Traverse adjacency lists to fill indegrees of
+        // vertices. This step takes O(V+E) time
+        for (int i = 0; i < numberOfVertices; i++) {
+            for (int node : adjacencies[i]) {
+                inDegree[node]++;
+            }
+        }
+
+        // Create a queue and enqueue all vertices with
+        // indegree 0
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < numberOfVertices; i++) {
+            if (inDegree[i] == 0)
+                q.add(i);
+        }
+
+        // Initialize count of visited vertices
+        int cnt = 0;
+
+        // Create a stack to store result (A topological
+        // ordering of the vertices)
+        Stack topOrder = new Stack();
+        while (!q.isEmpty()) {
+            // Extract front of queue (or perform dequeue)
+            // and add it to topological order
+            int u = q.poll();
+            topOrder.add(u);
+
+            // Iterate through all its neighbouring nodes
+            // of dequeued node u and decrease their in-degree
+            // by 1
+            for (int node : adjacencies[u]) {
+                // If in-degree becomes zero, add it to queue
+                if (--inDegree[node] == 0)
+                    q.add(node);
+            }
+            cnt++;
+        }
+
+        // Check if there was a cycle
+        if (cnt != numberOfVertices) {
+            throw new RuntimeException("A cycle was found in the Graph.");
+        }
+
+        return topOrder;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,30 @@ public class Main {
     {
         GraphFactory factory = new GraphFactory();
         // Create a graph with nodes.
-        Graph g = factory.createGraph(100000, 200005);
-
+        Graph g = factory.createGraph(10000, 20000);
         System.out.println("Following is a Topological " +
-                "sort of the given graph");
-        // Do a topological sort of the elements in the graph.
-        Stack sortedStack = g.topologicalSort();
-        while (!sortedStack.empty()) {
-            System.out.print(sortedStack.pop() + ", ");
+                "sort of the given graph using Khan's algorithm");
+        // Note: Topological sort using Khan returns a similar stack to depth-first, except that they are turned around.
+        // So khan's "0, 1, 2" becomes "2, 1, 0" when using depth-first.
+        long startTime = System.nanoTime();
+        Stack sortedStackKhan  = g.topologicalSortKhan();
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println("Khan ran in: " + duration + " nanoseconds or " + duration / 1000000 + " ms");
+
+        startTime = System.nanoTime();
+        Stack sortedStackDepth = g.topologicalSort();
+        endTime = System.nanoTime();
+        duration = (endTime - startTime);
+        System.out.println("Depth-first ran in: " + duration + " nanoseconds or " + duration / 1000000 + " ms");
+        while (!sortedStackKhan.empty()) {
+            System.out.print(sortedStackKhan.pop() + ", ");
+        }
+        // Formatting println.
+        System.out.println();
+        System.out.println("Now using Depth-first:");
+        while (!sortedStackDepth.empty()) {
+            System.out.print(sortedStackDepth.pop() + ", ");
         }
     }
 }
